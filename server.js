15b651cd7e0df81ef8b114b893c78f0837315e80
Raj Chaurasiya <102717203+TheRaj200@users.noreply.github.com>
2025-09-04 20:46:34 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const app = require('./src/app');
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const generateResponse = require("./src/services/ai.service");
+
+const chatHistory = []
+
+const httpServer = createServer(app);
+const io = new Server(httpServer,{
+  cors:{
+    origin: "http://localhost:5173",
+  }
+});
+
+io.on("connection", (socket) => {
+   console.log("a user connected");
+
+   socket.on("disconnect",()=>{
+    console.log("a user disconnected");
+   })
+
+   socket.on("ai-message", async (data)=>{
+    chatHistory.push({
+      role:"user",
+      parts:[{text:data}]
+    })
+
+     const response = await generateResponse(chatHistory);
+
+     chatHistory.push({
+      role:"model",
+      parts:[{text:response}]
+     });
+      socket.emit("ai-response", {response})
+    })
+});
+
+
+httpServer.listen(3000, ()=> {
+    console.log('Server is running on port 3000');
+})
